73ad39bc6408e8534d9cbdc1a2035623390ebb51
softawie <softawie@gmail.com>
2022-03-22 19:42:59 +0300

    Remove the now ignored build

--- build/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Data = void 0;
-exports.Data = ['encenadaport', 'fjord', 'icelandwaterfall', 'palmtunnel', 'santamonica'];
